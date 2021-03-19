749552f9af34b8914849e1e98504c07c4cfc2f04
ossi1801 <ossi1801@student.hamk.fi>
2021-03-19 12:58:20 +0200

    coinpickup

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Pickup();
+        }
+    }
+
+    void Pickup()
+    {
+        Debug.Log("picked");
+        Destroy(gameObject);
+    }
+}

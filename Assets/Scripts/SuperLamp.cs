f3f99aa4fe70ab8b60013a3f6e8edaafebc893e5
ossi1801 <ossi1801@student.hamk.fi>
2021-03-23 20:46:23 +0200

    spawnpoints-spawnpointAdder-FixesForHitbox-EnemyDeathFunction

--- Assets/Scripts/SuperLamp.cs
+++ Assets/Scripts/SuperLamp.cs
@@ -8,39 +8,67 @@ public class SuperLamp : MonoBehaviour
     public bool superLampIsOn = false;
     // public Transform enemyBody; //useless ? 
     public GameObject enemyBody;
+    public GameObject newEnemyPos1;
+    public GameObject newEnemyPos2;
+    public GameObject newEnemyPos3;
+    public float enemyRespawnTimer = 0.5f;
+    public List<Vector3> spawnpoints;
 
+
+    private void Start()
+    {
+     Vector3 newEnemyVec1 = newEnemyPos1.transform.position;
+     Vector3 newEnemyVec2 = newEnemyPos2.transform.position;
+     Vector3 newEnemyVec3 = newEnemyPos3.transform.position;
+    
+    spawnpoints.Add(newEnemyVec1);
+    spawnpoints.Add(newEnemyVec2);
+    spawnpoints.Add(newEnemyVec3);
+    }
 
     //https://answers.unity.com/questions/753481/ontriggerenter-not-working-tried-everything-c.html
     private void OnTriggerEnter(Collider enemyCollision)
     { 
         if (enemyCollision.CompareTag("Enemy") && superLampIsOn==true) //requires rigidbody component for ai
         {
-            enemyDeath();  
+            Debug.Log("Enemy destroyed / moved");
+            enemyBody.transform.position = new Vector3(56, 2, -43);  //THROW CUBE somewhere off map 
+            Invoke("enemyDeath", enemyRespawnTimer);  //and wait for timer amount the spawn to new location
         }
+       
     }
 
     void enemyDeath()
     {
-        Debug.Log("enemyhit!");
-        Destroy(enemyBody); //delete npc
-            // TODO add to new pos
+        Debug.Log("Enemy spawned");
+        //Destroy(enemyBody); //delete npc - bugs somehow ? new clone does not work with out the original or stmh
+        int spAmount =  spawnpoints.Count;
+        int nextSP = Random.Range(0, spAmount);
+        enemyBody.transform.position = spawnpoints[nextSP];
+       //Instantiate(enemyBody, spawnpoints[nextSP], transform.rotation);   //spawns new clone of enemy
     }
 
     // Update is called once per frame
     void Update()
     {
+        //CURRENT BUGS IF IN ZONE IN COLLIDER ZONE LIGHT != EFFECT ENEMY!!!!!!!!!!!!!!!!! 
+        //THIS NEEDS FIX!!
+
         //Input.getdown   edit -> Project settings -> Input manager  -> Axes all default input buttons
         if (Input.GetButtonDown("Fire1"))
-        {
-            if (fuel > 0f) { superLampIsOn = true; }
-            else
             {
-                superLampIsOn = false;
-                // Debug.Log(fuel.ToString());
+                Debug.Log(fuel.ToString());  //TODO SHOW UI WITH LAMP FUEL
+                if (fuel > 0f) 
+                { 
+                    superLampIsOn = true; 
+                    fuel -= 1*Time.deltaTime;
+                }
+                else  {superLampIsOn = false;}   
             }
 
-            if (fuel > 0) { fuel -= Time.deltaTime; } //fuel cant go to lesser than 0 
-   
+        if (Input.GetButtonUp("Fire1"))
+        {
+            superLampIsOn = false;
         }
   
     }

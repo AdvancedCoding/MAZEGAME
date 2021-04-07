03097731998ae38f9442dda939c12d1fc1ef5104
ossi1801 <ossi1801@student.hamk.fi>
2021-04-07 19:39:06 +0300

    Shop Collider and Ui button fixes or smth

---
+++ Assets/Scripts/ShopScript.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Inventory Inventory;
+    private bool ShopEnabled;
+    public GameObject ShopUIObject;
+    public GameObject ShopToolTip;
+    public bool playerIsInShop = false;
+  //  public Collider ShopCollider;
+
+    public void BuyFuel()
+    {
+        Inventory.UpdateInventory("fuel", 60);
+        Inventory.UpdateInventory("gold", -1);
+        Debug.Log("IM A GENIIUS");
+    }
+
+    public void Start()
+    {
+         ShopToolTip.SetActive(false);
+         ShopUIObject.SetActive(false);
+    }
+
+
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //show ui tip press e to open store
+            ShopToolTip.SetActive(true);
+            playerIsInShop = true;
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+           
+            playerIsInShop = false;
+        }
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P) && playerIsInShop)
+        {
+            ShopEnabled = !ShopEnabled;
+        }
+
+        if (ShopEnabled == true)
+        {
+
+            Cursor.lockState = CursorLockMode.Confined; //cursor locked to screen 
+            ShopUIObject.SetActive(true);
+            ShopToolTip.SetActive(false);
+        }
+
+        if (!ShopEnabled && playerIsInShop)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            ShopUIObject.SetActive(false);
+            ShopToolTip.SetActive(true);
+        }
+        if (!playerIsInShop) {
+            Cursor.lockState = CursorLockMode.Locked;
+            ShopUIObject.SetActive(false); 
+            ShopToolTip.SetActive(false);
+            ShopEnabled = false;
+        }
+
+       //cursor locked to screen 
+     
+
+    }
+
+
+
+}

513c81bf2d906b8ebcd06899e202f36518564113
OttoLeivo <0tt0.l31v0@gmail.com>
2021-04-08 17:36:50 +0300

    responsive-gold-sfx-fix

--- Assets/Scripts/playagain.cs
+++ Assets/Scripts/playagain.cs
@@ -11,13 +11,16 @@ public string sceneName = "Assets/oton-kuutio";
 public AudioSource gunSfx;
 public AudioClip clickFx;
 
+
 public void PlayAgain()
 {
 gunSfx.PlayOneShot (clickFx);
+
     SceneManager.LoadScene(sceneName);
 }
 
 void Start() {
+
 Cursor.lockState = CursorLockMode.Confined;
 }
 

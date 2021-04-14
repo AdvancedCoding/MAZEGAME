4d465fd5249c7f1eea126103aba773e4fc378904
joona1808 <joona.olsoni@student.hamk.fi>
2021-04-14 14:15:52 +0300

    player HP + npc attack testing

--- Assets/Scripts/NpcAttack.cs
+++ Assets/Scripts/NpcAttack.cs
@@ -7,21 +7,58 @@ public class NpcAttack : MonoBehaviour
 {
     public string sceneName = "oton-kuutio";
     public bool playerImmortality = false;
-   // public Object resetToScene;
+    public int PlayerHP = 2; 
+    private int PlayerRemainingHP;
+
+    public GameObject BloodStain;
+    public AudioClip NpcAttackAudio; 
+
+    // public Object resetToScene;
     // Start is called before the first frame update
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.collider.tag == "Player" &&!playerImmortality) SceneManager.LoadScene(sceneName);
-    }
 
     void Start()
     {
-        
+        BloodStain.SetActive(false);
+        PlayerRemainingHP = PlayerHP;
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.collider.tag == "Player")
+        {
+         
+            if (PlayerRemainingHP == PlayerHP)
+            {
+                PlayerRemainingHP--;
+                BloodStain.SetActive(true);          
+                Debug.Log("HP: " + PlayerRemainingHP);
+                AudioSource.PlayClipAtPoint(NpcAttackAudio, gameObject.transform.position);
+           
+            }
+
+            else if (PlayerRemainingHP < PlayerHP)
+            {
+                SceneManager.LoadScene(sceneName);
+            }
+
+        }
+
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            BloodStain.SetActive(false);
+            Debug.Log("Escaped");
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 }

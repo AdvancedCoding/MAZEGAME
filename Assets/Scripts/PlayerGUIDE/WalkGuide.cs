e1eddfd69517e490cd6e2d833690e4144bb65fa7
OttoLeivo <0tt0.l31v0@gmail.com>
2021-04-17 14:53:08 +0300

    walkGuideUpgrade

--- Assets/Scripts/PlayerGUIDE/WalkGuide.cs
+++ Assets/Scripts/PlayerGUIDE/WalkGuide.cs
@@ -6,29 +6,27 @@ public class WalkGuide : MonoBehaviour
 {
 
 public GameObject thePlayer;
-public GameObject hernandezText;
+public GameObject guideText;
+public GameObject niceText;
 public GameObject inventory;
 public int walkCheck = 0;
 
-/*
-void OnTriggerEnter(){
-hernandezCam.SetActive(true);
-thePlayer.SetActive(false);
+void Start(){
+guideText.SetActive(true);
 }
-*/
+
 
 void OnTriggerExit(Collider other){
-if (walkCheck == 0){
 
-inventory.SetActive(true);
-hernandezText.SetActive(true); //show hernandez text
+if (walkCheck == 0){
+guideText.SetActive(false);
+niceText.SetActive(true); //show hernandez text
 walkCheck=1;
 StartCoroutine(FinishCut());
 
-
 IEnumerator FinishCut(){ //Kameran vaihto takaisin 6s jälkeen
-yield return new WaitForSeconds(6);
-hernandezText.SetActive(false); //hide hernandez text
+yield return new WaitForSeconds(2);
+niceText.SetActive(false); //hide hernandez text
 inventory.SetActive(true);
 }
 

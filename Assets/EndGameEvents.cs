34a5093d4929957d5278269d29cb2cfea4f3b710
OttoLeivo <0tt0.l31v0@gmail.com>
2021-05-04 15:26:59 +0300

    new-mariasfx-better-music-sync-end-fade-more-things-to-come

--- Assets/EndGameEvents.cs
+++ Assets/EndGameEvents.cs
@@ -5,20 +5,46 @@ using UnityEngine.Video;
 
 public class EndGameEvents : MonoBehaviour
 {
+	public AudioSource mariaSFX;
+	public AudioSource mariaSFX2;
+
     public GameObject MariaCanvas;
+    public Animator totw;
     public VideoPlayer Video;
+      public Animator fadeBlack;
+      public Animator creditA;
     
                   public void playMaria(){ //called from camera end-game-animation EVENT
-                
                    MariaCanvas.SetActive(true);  
                     Video.Play();
-
                 }
                     public void hideMaria(){ //called from camera end-game-animation EVENT
-                
                    MariaCanvas.SetActive(false);  
                     Video.Stop();
 
                 }
+                
+                
+                 public void sadHernandez(){ //called from camera end-game-animation EVENT
+                mariaSFX.Play();
+                }
+                
+                public void sadHernandezMariaSNGLE(){ //called from camera end-game-animation EVENT
+                mariaSFX2.Play();
+                }
+                
+                 public void fadeBlack1(){ //called from camera end-game-animation EVENT
+                fadeBlack.SetTrigger("fadeBlack");
+                }
+                
+                public void showCredits(){ //called from camera end-game-animation EVENT
+              //  creditA.SetTrigger("show");
+                }
+                
+                 public void showTotw(){ //called from camera end-game-animation EVENT
+               // totw.SetTrigger("show");
+                }
+
+
     
 }

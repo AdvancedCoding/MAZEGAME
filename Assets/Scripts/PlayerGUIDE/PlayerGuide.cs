2c341956bb009a547782b7e29c9c0c71633f62b6
ossi1801 <ossi1801@student.hamk.fi>
2021-05-04 12:23:42 +0300

    Format playerguide  & player collide check with if

--- Assets/Scripts/PlayerGUIDE/PlayerGuide.cs
+++ Assets/Scripts/PlayerGUIDE/PlayerGuide.cs
@@ -5,50 +5,54 @@ using UnityEngine;
 public class PlayerGuide : MonoBehaviour
 {
 
-public GameObject thePlayer;
-public GameObject hernandezCam;
-public GameObject hernandezText;
-public GameObject inventory;
-public GameObject cinematicBars;
-public int shopCheck = 0;
-public Inventory Oilinventory;
+    public GameObject thePlayer;
+    public GameObject hernandezCam;
+    public GameObject hernandezText;
+    public GameObject inventory;
+    public GameObject cinematicBars;
+    public int shopCheck = 0;
+    public Inventory Oilinventory;
 
-/*
-void OnTriggerEnter(){
-hernandezCam.SetActive(true);
-thePlayer.SetActive(false);
-}
-*/
+    /*
+    void OnTriggerEnter(){
+    hernandezCam.SetActive(true);
+    thePlayer.SetActive(false);
+    }
+    */
 
 
-void OnTriggerEnter (Collider other){	//kun pelaaja kollidee ekan kerran näyttää mistä löytyy hernandezin kauppa
+    void OnTriggerEnter(Collider other)
+    {   //kun pelaaja kollidee ekan kerran näyttää mistä löytyy hernandezin kauppa
+        if (other.CompareTag("Player")){
+            if (shopCheck == 0 && SuperLamp.fuel == 0)
+            {
+                hernandezCam.SetActive(true);
+                cinematicBars.SetActive(true);
+                thePlayer.SetActive(false);
+                inventory.SetActive(false);
+                hernandezText.SetActive(true); //show hernandez text
+                StartCoroutine(FinishCut());
+                shopCheck = 1;
 
-if (shopCheck == 0 && SuperLamp.fuel == 0){
-hernandezCam.SetActive(true);
-cinematicBars.SetActive(true);
-thePlayer.SetActive(false);
-inventory.SetActive(false);
-hernandezText.SetActive(true); //show hernandez text
-StartCoroutine(FinishCut());
-shopCheck=1;
+                IEnumerator FinishCut()
+                { //Kameran vaihto takaisin 6s jälkeen
+                    yield return new WaitForSeconds(6);
+                    thePlayer.SetActive(true);
+                    hernandezText.SetActive(false); //hide hernandez text
+                    cinematicBars.SetActive(false);
+                    hernandezCam.SetActive(false);
+                    inventory.SetActive(true);
+                }
 
-IEnumerator FinishCut(){ //Kameran vaihto takaisin 6s jälkeen
-yield return new WaitForSeconds(6);
-thePlayer.SetActive(true);
-hernandezText.SetActive(false); //hide hernandez text
-cinematicBars.SetActive(false);
-hernandezCam.SetActive(false);
-inventory.SetActive(true);
-}
+            }
 
-}
-
-else{
-Debug.Log("shop already seen");
-shopCheck=1;
-}
-
-}
+            else
+            {
+                Debug.Log("shop already seen");
+                shopCheck = 1;
+            }
+        }
+    }
 
 
 }
